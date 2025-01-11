82f61847741919f4a2ae69b9da955c7a8a600f19
RamanaBellary <ramanayadavs@gmail.com>
2025-01-11 18:44:10 +0530

    Checked-in Likes functionality changes

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using API.Data;
 using API.Extensions;
 using API.Middleware;
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
+import { LikesService } from '../../services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -10,5 +11,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './member-card.component.css'
 })
 export class MemberCardComponent {
+  private likeService = inject(LikesService);
   member = input.required<Member>();
+  hasLiked = computed(()=>this.likeService.likeIds().includes(this.member().Id));
+
+  toggleLike(){
+    this.likeService.toggleLike(this.member().Id).subscribe({
+      next: () => {
+        if(this.hasLiked()){
+          this.likeService.likeIds.update(ids => ids.filter(x=>x != this.member().Id))
+        }
+        else{
+          this.likeService.likeIds.update(ids => [...ids, this.member().Id])
+        }
+      }
+    })
+  }
 }
